2bedcec69e33208645e075c4b7fd8b540b843a2d
Jose Bascuñan <jose.bascunan.p@gmail.com>
2017-12-13 15:16:28 -0300

    cambios

--- 1.Introduccion/TestMyModule.py
+++ 1.Introduccion/TestMyModule.py
@@ -8,6 +8,8 @@ class TestMyModule(unittest.TestCase):
 
     def test_suma(self):
         print(myModule.sum(5, 7))
+        edad = int(input("Dígame su edad: "))
+        print("Su edad son", edad, "años")
 
 
 if __name__ == "__main__":
